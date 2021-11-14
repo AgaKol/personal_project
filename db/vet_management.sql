0d21eccee3f6edec58c2d44220a7b00b1ea2abd6
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-14 22:05:02 +0000

    vets table added

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -11,3 +11,13 @@ CREATE TABLE owners (
     email VARCHAR(255),
     registered BOOLEAN
 );
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    address VARCHAR(255),
+    phone_number VARCHAR(255),
+    email VARCHAR(255),
+    speciality VARCHAR(255)
+);
