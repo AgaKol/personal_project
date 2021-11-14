01d1ee1c872c9eb9bfd07a544993c1a9b1438b61
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-14 22:04:04 +0000

    animals table added

---
+++ db/vet_management.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS vets;
+DROP TABLE IF EXISTS owners;
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    address VARCHAR(255),
+    phone_number VARCHAR(255),
+    email VARCHAR(255),
+    registered BOOLEAN
+);
