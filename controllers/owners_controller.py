6b9c9f849e17246de954ddc0bddddafe6b11a7ec
Aga Koltun <agnieszka.koltun.89@gmail.com>
2021-11-16 21:14:48 +0000

    unregistered owners cannot add pets

--- controllers/owners_controller.py
+++ controllers/owners_controller.py
@@ -2,6 +2,7 @@ import re
 from flask import Flask, render_template, request, redirect, Blueprint
 from repositories import owner_repository, animal_repository, vet_repository, treatment_repository
 from models.owner import Owner
+from models.animal import Animal
 
 owners_blueprint = Blueprint("owners", __name__)
 
@@ -37,3 +38,13 @@ def update_owner(id):
     owner = Owner(name,address,phone,email,registered,id)
     owner_repository.update(owner)
     return redirect("/owners")
+
+@owners_blueprint.route("/owners/<id>/add_pet", methods = ["GET"])
+def adding_pet(id):
+    owner = owner_repository.select_one(id)
+    treatments = treatment_repository.select_all()
+    vets = vet_repository.select_all()
+    if owner.registered == False:
+        return render_template("owners/add_pet.html", owner = owner)
+    else:
+        return render_template("/pets/new.html", treatments = treatments, vets = vets)
